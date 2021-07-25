a7ea9885b870973302371b71c08b2d7ceaae59f6
Rajendra Khope <research@iocare.in>
2021-07-25 18:35:34 +0530

    update package.json

---
+++ scripts/build-after.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const util = require('util');
+const stat = util.promisify(fs.stat);
+const request = require('request');
+const path = require('path');
+
+module.exports = function (ctx) {
+  console.log(ctx);
+
+  if (!ctx.opts.platforms.includes('android')) return;
+
+  const platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
+  const apkFileLocation = path.join(platformRoot, 'app/build/outputs/apk/debug/app-debug.apk');
+
+  const formData = {
+    apk: fs.createReadStream(apkFileLocation),
+    key: 'RaskIONIC@2021'
+  };
+  request.post({ url: 'https://www.iocare.in/upload-apk', formData: formData }, function optionalCallback(err, httpResponse, body) {
+    if (err) {
+      return console.error('upload failed:', err);
+    }
+    console.log(body);
+  });
+
+  return stat(apkFileLocation).then(stats => {
+    console.log(`Size of ${apkFileLocation} is ${stats.size} bytes`);
+  });
+};
